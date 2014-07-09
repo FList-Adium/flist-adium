7302cf3c934c31c6762b41f52f358d702ef975e9
John Highwater <thevaultedmind@gmail.com>
2014-07-08 22:46:08 -0700

    Added notifications for new notes

---
+++ ESFlistInternalNoteTrampoline.h
@@ -0,0 +1,11 @@
+//
+//  ESFlistInternalNoteTrampoline.h
+//  F-Chat
+//
+//
+
+#import <Foundation/Foundation.h>
+#import <Adium/ESTextAndButtonsWindowController.h>
+
+@interface ESFlistInternalNoteTrampoline : NSObject
+@end
---
+++ ESFlistNoteTrampoline.h
@@ -0,0 +1,13 @@
+//
+//  ESFlistNoteTrampoline.h
+//  F-Chat
+//
+//
+
+#ifndef F_Chat_ESFlistNoteTrampoline_h
+#define F_Chat_ESFlistNoteTrampoline_h
+
+void SendFlistNote(char *message, char *url, PurpleConnection *pc);
+
+
+#endif
